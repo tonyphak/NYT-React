a1009ac470ec7476e02efbbb8085cfa35bd139cc
Tony Phakasoum <39785612+tonyphak@users.noreply.github.com>
2019-01-22 22:25:14 -0800

    Delete API.js

--- client/src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default {
-  getBooks: function(title) {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + title + "&key=AIzaSyAdfHFBZa4g248H4DSW-s-ckR6lCAzMpDY")
-  }
-};
